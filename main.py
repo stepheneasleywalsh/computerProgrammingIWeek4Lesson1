a54752418e8542ead6940c5418935651c1c4ce54
stepheneasleywalsh <stephen.easley-walsh@studygroup.com>
2021-10-22 21:29:38 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+x = int(input("How many chocolates do you have?"))
+N = int(input("How many friends do you have?"))
+f = x // N
+R = x % N
+print(" ")
+print("You will need to give")
+print(f)
+print("chocolates to your friends and you will have left")
+print(R)
