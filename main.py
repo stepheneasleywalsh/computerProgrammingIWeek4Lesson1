379e5090449b9ef762769899ff58284f7eacd034
stepheneasleywalsh <stephen.easley-walsh@studygroup.com>
2021-10-26 14:45:58 +0100

    Version 2

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-x = int(input("How many chocolates do you have?"))
-N = int(input("How many friends do you have?"))
-f = x // N
-R = x % N
-print(" ")
-print("You will need to give")
-print(f)
-print("chocolates to your friends and you will have left")
-print(R)
+print("The two roots of the quadratic x**2 + 2x - 5 are as follows...")
+
+#Calculate root one
+print((-2+(2**2-4*1*(-5))**0.5)/(2*1))
+
+#Calculate root two
+print((-2-(2**2-4*1*(-5))**0.5)/(2*1))
+
