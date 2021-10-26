9f29a687747d0ea46c38d340dee31c3c294d785c
stepheneasleywalsh <stephen.easley-walsh@studygroup.com>
2021-10-26 14:48:45 +0100

    I have added in a Goodbye message

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 print("The two roots of the quadratic x**2 + 2x - 5 are as follows...")
 
-#Calculate root one
+#The is the First Root
 print((-2+(2**2-4*1*(-5))**0.5)/(2*1))
 
-#Calculate root two
+#This is the Second Root
 print((-2-(2**2-4*1*(-5))**0.5)/(2*1))
 
+print("Goodbye")
